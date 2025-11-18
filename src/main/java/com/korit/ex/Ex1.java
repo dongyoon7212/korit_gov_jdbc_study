1210dff10af8dd37a02a8815be2657adb386589c
dongyoon7212 <dongyoon7212@naver.com>
2025-11-18 14:22:53 +0900

    Feat: ex

---
+++ src/main/java/com/korit/ex/Ex1.java
@@ -0,0 +1,28 @@
+package com.korit.ex;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int kor = 75;
+        int eng = 88;
+        int math = 92;
+
+        // 1. 총점 구하기
+        int sum = ;
+
+        // 2. 평균 구하기
+        double avg = ;
+
+        // 3. 최고점 구하기
+        int max = kor;
+        if (eng > max) {
+            max = eng;
+        }
+        if (math > max) {
+            max = ;
+        }
+
+        System.out.println("총점: " + sum);
+        System.out.println("평균: " + avg);
+        System.out.println("최고점: " + max);
+    }
+}
---
+++ src/main/java/com/korit/ex/Ex2.java
@@ -0,0 +1,27 @@
+package com.korit.ex;
+
+public class Ex2 {
+    public String name;
+    public int age;
+
+    // 1. 생성자
+    public Ex2(String name, int age) {
+        //
+    }
+
+    // 2. 정보 출력 메서드
+    public void printInfo() {
+        System.out.println("이름: " + name + ", 나이: " + age);
+    }
+
+    // 3. 성인 여부 확인 (20세 이상이면 true)
+    public boolean isAdult() {
+        //
+    }
+
+    public static void main(String[] args) {
+        Ex2 m = new Ex2("홍길동", 23);
+        m.printInfo();
+        System.out.println("성인 여부: " + m.isAdult());
+    }
+}
